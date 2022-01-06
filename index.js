6588700c6ca17515ef8466f21636fcf281d8ef3b
Bruce <brucecheung@users.noreply.github.com>
2022-01-06 14:29:08 +0000

    Respond with sender action typing_on - 6

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ function startEngineMove(fen, senderId) {
 
 async function postEngineMove(engineMove) {
 	if (isEngineRunning) {
-		//typingOn(senderId).then();
+		typingOn(engineProcessingSenderId);
 		await new Promise(r => setTimeout(r, 3000));
 		
 		makeMove(engineProcessingSenderId, engineMove)
